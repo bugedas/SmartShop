668a8cd8275517e1af07d99c317a7f9369b636d0
JustasGl <35509569+JustasGl@users.noreply.github.com>
2021-05-14 01:46:26 +0300

    Prekės kortelės UI facelift

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import ProductForm from './ProductForm'
 import {deleteProduct} from "./PruductFunctions";
 import '../css/ProductStyles.css'
@@ -89,7 +90,12 @@ const Product = (props) => {
                     <IconButton aria-label="delete" onClick={() =>onDeleteClick(props.id)}>
                         <DeleteIcon />
                     </IconButton>
-                    
+
+                    <Link to={"/MakeOrder/"+props.id}>
+                    <IconButton aria-label="AddShoppingCart">
+                    <AddShoppingCartIcon />
+                    </IconButton>
+                    </Link>
                 </CardActions>
 
                 <div style={{display: (isEdit ? 'block' : 'none')}}>
